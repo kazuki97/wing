503151ad5e2d1f67e5cff761570ca41be99f3e0e
kazuki97 <kazuki_09.07@icloud.com>
2024-08-15 21:24:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -430,7 +430,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function displaySales() {
         const year = yearFilter.value;
-        const month = monthFilter.value;
+        const month = monthFilter.value.padStart(2, '0');
 
         const transaction = db.transaction(['sales'], 'readonly');
         const store = transaction.objectStore('sales');
