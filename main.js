ab65c1c2afde1803ffa958a530b41a99f7d65644
kazuki97 <kazuki_09.07@icloud.com>
2024-10-02 01:01:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
+// main.js
+
 import { initializeUI } from './ui.js';
 import { initializeDatabase, deleteDatabase } from './db.js';
 import { showErrorModal } from './errorHandling.js';
 import { initializeCategories } from './categories.js';
-// 分割後のファイルに置き換える
 import { initializeInventorySection } from './inventoryManagement.js';
 
 // ページが読み込まれたときにデータベースを初期化
-document.addEventListener('DOMContentLoaded', () => {
-    initializeDatabase()
-        .then(() => {
-            initializeUI();
-            initializeCategories(); // カテゴリの初期化を呼び出す
-            initializeInventorySection(); // 在庫管理セクションの初期化を呼び出す
-        })
-        .catch((error) => {
-            console.error('Database initialization failed:', error);
-            // エラーモーダルを表示
-            showErrorModal('データベースの初期化に失敗しました。アプリケーションを再読み込みしてください。');
-        });
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        await initializeDatabase();
+        initializeUI();
+        initializeCategories();
+        initializeInventorySection();
+    } catch (error) {
+        console.error('Database initialization failed:', error);
+        showErrorModal('データベースの初期化に失敗しました。アプリケーションを再読み込みしてください。');
+    }
 });
 
 // データベースリセットボタンの処理
-document.getElementById('reset-database').addEventListener('click', () => {
+document.getElementById('reset-database').addEventListener('click', async () => {
     if (confirm('本当にデータベースをリセットしますか？すべてのデータが削除されます。')) {
-        deleteDatabase()
-            .then(() => {
-                console.log('データベースがリセットされました。');
-                alert('データベースが正常にリセットされました。ページを再読み込みしてください。');
-            })
-            .catch((error) => {
-                console.error('データベースのリセット中にエラーが発生しました:', error);
-                showErrorModal('データベースのリセット中にエラーが発生しました。');
-            });
+        try {
+            await deleteDatabase();
+            console.log('データベースがリセットされました。');
+            alert('データベースが正常にリセットされました。ページを再読み込みしてください。');
+        } catch (error) {
+            console.error('データベースのリセット中にエラーが発生しました:', error);
+            showErrorModal('データベースのリセット中にエラーが発生しました。');
+        }
     }
 });
