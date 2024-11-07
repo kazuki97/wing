7c9cc8e75c193dae0490f9613793beeeb37540b9
kazuki97 <kazuki_09.07@icloud.com>
2024-11-08 03:01:12 +0900

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,13 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.14.0/firebas
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
 
 const firebaseConfig = {
-  // あなたのFirebaseプロジェクトの設定情報
+  apiKey: "AIzaSyBND1iUtwW9-D3wisl5-wO_-Sfahab0zmA",
+  authDomain: "wing-system.firebaseapp.com",
+  projectId: "wing-system",
+  storageBucket: "wing-system.appspot.com",
+  messagingSenderId: "308022918518",
+  appId: "1:308022918518:web:ebdfe5deb82fa32a3f4e57",
+  measurementId: "G-806S4XB84E"
 };
 
 // Firebaseアプリの初期化
