b049173de5f6b95f2b276e0633d778c414d33813
kazuki97 <kazuki_09.07@icloud.com>
2024-11-08 11:38:32 +0900

    Update db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBND1iUtwW9-D3wisl5-wO_-Sfahab0zmA",
   authDomain: "wing-system.firebaseapp.com",
   projectId: "wing-system",
-  storageBucket: "wing-system.appspot.com",
+  storageBucket: "wing-system.firebasestorage.app",
   messagingSenderId: "308022918518",
   appId: "1:308022918518:web:ebdfe5deb82fa32a3f4e57",
   measurementId: "G-806S4XB84E"
